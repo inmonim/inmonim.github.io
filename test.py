8d11bb05582cde4eb37abc325e3b2abb5b06739e
inmonim <cat1083014@gmail.com>
2024-01-27 17:28:10 +0900

    change code-block css

--- test.py
+++ test.py
@@ -5,3 +5,14 @@ origin_list = [a, 5, 3]
 print(origin_list)
 
 print(origin_list[0] is a)
+
+b= 0
+a * b
+
+int()
+
+def abc(a :int) -> int:
+    for i in range(a):
+        print(a)
+
+# fdsafaafsdfads
